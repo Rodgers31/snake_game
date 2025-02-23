1ba46047e051dbfaa47868570a0384b76f0d66c0
Rodgers31 <otienor986@gmail.com>
2025-02-23 12:01:32 -0600

    snake movement

--- snake.py
+++ snake.py
@@ -9,6 +9,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
 
@@ -28,5 +29,17 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
 
         # moving the first segment
-        self.segments[0].forward(MOVE_DISTANCE)
-        self.segments[0].left(90)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
+
