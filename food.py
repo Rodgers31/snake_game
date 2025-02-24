164b2b690f6d7e323efe7067fe69da0a9fb53219
Rodgers31 <otienor986@gmail.com>
2025-02-23 20:29:34 -0600

    spell

--- food.py
+++ food.py
@@ -8,7 +8,7 @@ class Food(Turtle):
         super().__init__()
         self.shape("circle")
         self.penup()
-        # Halfing the turtle
+        # Halving the turtle
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
